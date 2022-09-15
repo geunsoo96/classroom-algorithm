648996605fe79e03606e4eab68766783f69a2326
Kimgeunsoo <geunsoo1106@gmail.com>
2022-09-15 14:38:17 +0900

    아이스크림 계산하기

---
+++ algothm6.js
@@ -0,0 +1,8 @@
+// Q. KDT 305호실 전원은 강사포함 29명이다. 좋은날씨에 비가 갑자기 오다가 다시 더워져 아이스크림을 사먹기로 했다. 건너 주유소 편의점에서 아이스크림을 찾아보니 1000원 짜리가 알맞게 진열되어있다. 암산해보니 2만9천원이라는 계산이 나온다. 이것을 문서에 출력할 후 있을까?
+
+let member = [];
+member.length = 29;
+let iceCream = 1000;
+let bboombbye = member.length * iceCream;
+
+console.log(bboombbye);
